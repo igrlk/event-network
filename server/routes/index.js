c025837110e5b36ec9a01b112844d43e49c0c67e
Игорь Лученков <igorlucenkov@MBP-Igor.Dlink>
2019-07-22 13:36:24 +0300

    add events

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,38 @@
 const authRoutes = require('./authRoutes');
+const eventRoutes = require('./eventRoutes');
+const User = require('../models/user');
 
 module.exports = app => {
   app.use('/auth', authRoutes);
+
+  app.use('', addUserToRequest);
+
+  app.use('/events', eventRoutes);
 };
+
+async function addUserToRequest(req, res, next) {
+  try {
+    const authHeader = req.get('Authorization');
+    if (!authHeader || authHeader.split(' ')[0].toLowerCase() !== 'bearer') {
+      throw new Error({
+        statusCode: 403,
+        message: 'Invalid token',
+        data: [],
+      });
+    }
+    const token = authHeader.split(' ')[1];
+
+    const user = await User.findByToken(token);
+    if (!user) {
+      throw new Error({
+        statusCode: 403,
+        message: 'Invalid token',
+        data: [],
+      });
+    }
+    req.user = user;
+    next();
+  } catch (err) {
+    next(err);
+  }
+}
